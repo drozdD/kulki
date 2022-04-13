3e8fbe85753556b1815004c3614956e5e1ff40d4
Jakub Drozd <jdrozd2003@gmail.com>
2022-04-13 15:56:13 +0200

    setup i guess

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+class Main {
+    static renderGameField() {
+        var container = document.getElementById('container')
+        for (var i = 0; i < 9; i++) {
+            for (var j = 0; j < 9; j++) {
+
+            }
+        }
+    }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,33 @@
+// const path = require('path');
+// module.exports = {
+//     entry: './src/plik_wejsciowy.js',
+//     output: {
+//         path: path.resolve(__dirname, './dist'),
+//         filename: 'bundle.js'
+//     },
+//     watch: true
+// };
 const path = require('path');
 module.exports = {
-    entry: './src/plik_wejsciowy.js',
+    entry: {
+        plik1: './src/main.ts',
+        plik2: './src/plik2.ts'
+    },
     output: {
         path: path.resolve(__dirname, './dist'),
-        filename: 'bundle.js'
+        filename: '[name].bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
     },
     watch: true
-};
+}
